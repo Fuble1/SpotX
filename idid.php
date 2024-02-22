4eaefed12dd3a7b269dd677bfe8916e6a214f0c8
Blink221 <120723054+Blink221@users.noreply.github.com>
2024-02-22 13:10:26 +0100

    Update idid.php

--- idid.php
+++ idid.php
@@ -46,13 +46,31 @@
             "img"=>"../img/trevis.jfif",
             "titolo"=>"GooseBumps",
             "autore"=>"Travis Scott",
-            "audio"=>"../songs/trevis.mp3"
+            "audio"=>"../songs/tonyboy.mp3"
         ),
         array(
             "img"=>"../img/rondo.jfif",
             "titolo"=>"Piccola Star",
             "autore"=>"RondoDaCorsa",
-            "audio"=>"../songs/rondo.mp3"
+            "audio"=>"../songs/tonyboy.mp3"
+        ),
+        array(
+            "img"=>"../img/star.jfif",
+            "titolo"=>"StarBoy",
+            "autore"=>"The weekend",
+            "audio"=>"../songs/tonyboy.mp3"
+        ),
+        array(
+            "img"=>"../img/maroon.jfif",
+            "titolo"=>"Cold",
+            "autore"=>"Marrons 5",
+            "audio"=>"../songs/tonyboy.mp3"
+        ),
+        array(
+            "img"=>"../img/hip.jfif",
+            "titolo"=>"HipHop Chilling",
+            "autore"=>"Flinkies",
+            "audio"=>"../songs/tonyboy.mp3"
         )
     );
 
@@ -63,7 +81,7 @@
             <h3><?php echo($el["titolo"]) ?></h3>
             <p><?php echo($el["autore"]) ?></p>
             <audio id="audioPlayer<?php echo $key + 1 ?>" controls style="display: none;">
-                <source src="<?php echo $el["../songs/tonyboy.mp3"] ?>" type="audio/mp3">
+                <source src="<?php echo $el["audio"] ?>" type="audio/mp3">
             </audio>
             <button onclick="toggleAudioPlayer('audioPlayer<?php echo $key + 1 ?>')" class="btn-play">Play</button>
         </div>
@@ -111,9 +129,6 @@
         </section>
     </main>
 
-    <footer>
-        <p>&copy; 2024 Spotix. Tutti i diritti riservati.</p>
-    </footer>
 
     <script>
         function toggleAudioPlayer(playerId) {
@@ -162,7 +177,12 @@
     </script>
     <script src="scripts.js"></script>
     <footer>
-        <p>&copy; 2024 MyMusic. Tutti i diritti riservati.</p>
+        <p>&copy; 2024 SpotX. Tutti i diritti riservati.</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+</svg>
+    <p> <i class="fab fa-github"></i> <a href="https://github.com/fuble1">GitHub</a></p>
+        
     </footer>
 </body>
 </html>
