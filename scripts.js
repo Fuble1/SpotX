e3c18d1675584c490bbcb7295832c4f32fcf13a3
Blink221 <120723054+Blink221@users.noreply.github.com>
2024-02-28 15:52:03 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,44 @@
-   function toggleAudioPlayer(playerId) {
-            var audioPlayer = document.getElementById(playerId);
-            var button = audioPlayer.nextElementSibling;
-            
-            // Chiudi tutti i lettori audio aperti tranne quello attuale
-            var allAudioPlayers = document.querySelectorAll('audio');
-            allAudioPlayers.forEach(function(player) {
-                if (player.id !== playerId) {
-                    player.style.display = 'none';
-                    player.pause();
-                    // Riporta il testo del pulsante "Play" per gli altri player
-                    player.nextElementSibling.textContent = 'Play';
-                }
-            });
-
-            // Apri/chiudi il lettore audio attuale
-            if (audioPlayer.style.display === 'none') {
-                audioPlayer.style.display = 'block';
-                audioPlayer.play();
-                button.textContent = 'Pause';
-            } else {
-                audioPlayer.style.display = 'none';
-                audioPlayer.pause();
-                button.textContent = 'Play';
-            }
+// Funzione per cercare le canzoni in base al titolo
+function searchSongs() {
+    var input, filter, songs, song, title, i;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    songs = document.getElementsByClassName('song');
+    
+    for (i = 0; i < songs.length; i++) {
+        song = songs[i];
+        title = song.getElementsByTagName('h3')[0].textContent.toUpperCase();
+        if (title.indexOf(filter) > -1) {
+            song.style.display = "";
+        } else {
+            song.style.display = "none";
         }
+    }
+}
+
+function toggleAudioPlayer(playerId) {
+    var audioPlayer = document.getElementById(playerId);
+    var button = audioPlayer.nextElementSibling;
+    
+    // Chiudi tutti i lettori audio aperti tranne quello attuale
+    var allAudioPlayers = document.querySelectorAll('audio');
+    allAudioPlayers.forEach(function(player) {
+        if (player.id !== playerId) {
+            player.style.display = 'none';
+            player.pause();
+            // Riporta il testo del pulsante "Play" per gli altri player
+            player.nextElementSibling.textContent = 'Play';
+        }
+    });
+
+    // Apri/chiudi il lettore audio attuale
+    if (audioPlayer.style.display === 'none') {
+        audioPlayer.style.display = 'block';
+        audioPlayer.play();
+        button.textContent = 'Pause';
+    } else {
+        audioPlayer.style.display = 'none';
+        audioPlayer.pause();
+        button.textContent = 'Play';
+    }
+}
